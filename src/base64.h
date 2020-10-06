47a84790ae3a93da1ffabf5d021545a1dda44503
Vova Abdrakhmanov <369565@gmail.com>
2020-10-06 11:13:14 +0400

    Improve CMakeLists.txt

--- src/base64.h
+++ src/base64.h
@@ -6,4 +6,3 @@ uint32_t get_encoded_length(uint32_t data_len);
 uint32_t get_decoded_length(uint32_t data_len);
 int encode(const char *input, uint32_t input_length, char *output);
 int decode(const char *input, uint32_t input_length, char *output);
-
