27e306ff140da65890314730ba557c7faacb6a88
Vova Abdrakhmanov <369565@gmail.com>
2020-10-07 23:49:10 +0400

    Format code

--- examples/bench.c
+++ examples/bench.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
 #include "base64.h"
 
 int main(int argc, char *argv[]) {
   for (int i = 1000; i < 100000; i *= 10) {
     for (int j = 0; j < 10; ++j) {
       char *input = malloc(i);
-      char *output = malloc(get_encoded_length(i)+1);
+      char *output = malloc(get_encoded_length(i) + 1);
       char *check = malloc(i);
-      FILE* fd = fopen("/dev/urandom", "rb");
+      FILE *fd = fopen("/dev/urandom", "rb");
       fread(input, 1, i, fd);
 
       double start = (double)clock() / CLOCKS_PER_SEC;
