94372b79758d28df4373a4cd1608540cbd988250
Vova Abdrakhmanov <369565@gmail.com>
2020-10-08 18:27:02 +0400

    Add extern C

--- src/base64.h
+++ src/base64.h
@@ -2,7 +2,15 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint32_t get_encoded_length(uint32_t data_len);
 uint32_t get_decoded_length(uint32_t data_len);
 int encode(const char *input, uint32_t input_length, char *output);
 int decode(const char *input, uint32_t input_length, char *output);
+
+#ifdef __cplusplus
+}
+#endif
