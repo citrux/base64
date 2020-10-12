06a532f8fc8aac0d623a0d4703ccc3831c9eec16
Vova Abdrakhmanov <369565@gmail.com>
2020-10-12 23:00:52 +0400

    Improve decode performance

--- src/base64.c
+++ src/base64.c
@@ -3,6 +3,15 @@
 const char alphabet[64] =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+const uint8_t rev_alphabet[128] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, -1, -1, -1, -1, -1, -1, -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1,
+    -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+    43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1};
+
 char get_alphabet_symbol(uint8_t index) {
   if (index > 63) {
     return -1;
@@ -11,12 +20,10 @@ char get_alphabet_symbol(uint8_t index) {
 }
 
 int8_t get_alphabet_index(char symbol) {
-  for (int i = 0; i < 64; ++i) {
-    if (alphabet[i] == symbol) {
-      return i;
-    }
+  if (symbol < 0) {
+    return -1;
   }
-  return -1;
+  return rev_alphabet[symbol];
 }
 
 uint32_t get_encoded_length(uint32_t data_len) {
