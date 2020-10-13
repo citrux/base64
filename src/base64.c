f8e66d7cc2c33e042be70f84ec90f04212f8b1d0
Vova Abdrakhmanov <369565@gmail.com>
2020-10-13 20:30:30 +0400

    Improve encode performance

--- src/base64.c
+++ src/base64.c
@@ -33,43 +33,35 @@ uint32_t get_encoded_length(uint32_t data_len) {
 uint32_t get_decoded_length(uint32_t data_len) { return data_len * 3 / 4; }
 
 int encode(const char *input, uint32_t input_length, char *output) {
-  uint8_t bits_in_buffer = 0;
-  uint16_t buffer = 0;
-  uint32_t read_position = 0;
-  uint32_t write_position = 0;
-  while (read_position < input_length || bits_in_buffer >= 6) {
-    if (bits_in_buffer < 6) {
-      uint16_t chunk = (uint8_t)input[read_position++];
-      buffer |= chunk << (8 - bits_in_buffer);
-      bits_in_buffer += 8;
-    }
-    char c = get_alphabet_symbol(buffer >> 10);
-    if (c == -1) {
-      return -1;
-    }
-    output[write_position++] = c;
-    buffer <<= 6;
-    bits_in_buffer -= 6;
+  for (int read_position = 0; read_position < input_length - 2; read_position += 3) {
+    uint32_t buffer = 0;
+    buffer |= ((uint8_t)input[read_position]) << 16;
+    buffer |= ((uint8_t)input[read_position + 1]) << 8;
+    buffer |= ((uint8_t)input[read_position + 2]);
+
+    int write_position = read_position / 3 * 4;
+    output[write_position++] = get_alphabet_symbol((buffer >> 18) & 0x3f);
+    output[write_position++] = get_alphabet_symbol((buffer >> 12) & 0x3f);
+    output[write_position++] = get_alphabet_symbol((buffer >> 6) & 0x3f);
+    output[write_position++] = get_alphabet_symbol(buffer & 0x3f);
   }
-  if (bits_in_buffer) {
-    char c = get_alphabet_symbol(buffer >> 10);
-    if (c == -1) {
-      return -1;
-    }
-    switch (bits_in_buffer) {
-    case 2:
-      output[write_position++] = c;
+  int write_position = input_length / 3 * 4;
+  switch (input_length % 3){
+    case 1:
+      output[write_position++] = get_alphabet_symbol((uint8_t)input[input_length-1] >> 2);
+      output[write_position++] = get_alphabet_symbol(((uint8_t)input[input_length-1] << 4) & 0x3f);
       output[write_position++] = '=';
       output[write_position++] = '=';
       break;
-    case 4:
-      output[write_position++] = c;
+    case 2:
+      output[write_position++] = get_alphabet_symbol(((uint8_t)input[input_length-2]) >> 2);
+      output[write_position++] = get_alphabet_symbol((((uint8_t)input[input_length-2]) << 4) & 0x3f | (((uint8_t)input[input_length-1]) >> 4) & 0x3f);
+      output[write_position++] = get_alphabet_symbol((((uint8_t)input[input_length-1]) << 2) & 0x3f);      
       output[write_position++] = '=';
       break;
     default:
       break;
     }
-  }
   return write_position;
 }
 
