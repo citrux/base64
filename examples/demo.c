a1ed9ee596f60417b715485126851cffc43440dc
Vova Abdrakhmanov <369565@gmail.com>
2022-12-23 12:58:17 +0300

    fix: remove memset after calloc

--- examples/demo.c
+++ examples/demo.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[]) {
 
   // 2. Allocate output buffer (+1 for '\0' at the end)
   char *encoded = calloc(enc_len + 1, sizeof(char));
-  memset(encoded, 0, enc_len + 1);
 
   // 3. Encode
   base64_encode(input, strlen(input), encoded);
@@ -33,7 +32,6 @@ int main(int argc, char *argv[]) {
   
   // 3. Allocate output buffer (+1 for '\0' at the end)
   char *decoded = calloc(dec_len + 1, sizeof(char));
-  memset(decoded, 0, dec_len + 1);
 
   // 4. Decode
   dec_len = base64_decode(encoded, strlen(encoded), decoded);
